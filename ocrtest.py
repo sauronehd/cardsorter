845943799807e0b9e11f2fee4f130b208a8c6c9d
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-04-07 13:55:09 -0700

    update test to be more failsafe

--- ocrtest.py
+++ ocrtest.py
@@ -2,14 +2,16 @@ import pytesseract
 from PIL import Image
 import os
 import os
-
+import sys
 if os.name == "posix":
     print("Importing pi camera2")
     from picamera2 import Picamera2
 else:
-    print("Not importing picamera2")
+    print("Not importing picamera2, quitting")
+    sys.exit(1)
 
 try:
+    wait = input("Enter to start")
     camera = Picamera2()
     camera.start_and_capture_file("temp.jpg")
     image = Image.open("temp.jpg")
