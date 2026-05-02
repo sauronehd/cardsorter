76165bb67bf92b929ae9b2943cb1be934f5555ad
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-05-02 13:50:58 -0700

    checking tresh

--- testing2.py
+++ testing2.py
@@ -2,7 +2,7 @@ from ocrfunc import ocr
 from picamera2 import Picamera2, Preview
 from PIL import Image
 import time
-
+import cv2
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
@@ -17,4 +17,7 @@ picam2.capture_file("temp.jpg")
 
 image = Image.open("temp.jpg")
 #print(image.shape)
+img = cv2.imread("image.png", 0)
+_, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+cv2.imwrite("thresh.png", thresh)
 print(f"The image text is:{ocr(image)}")
