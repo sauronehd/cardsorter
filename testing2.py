573e1e214b0575158e89eac747057ffffc5df1e6
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-05-02 14:00:23 -0700

    aiting for file now

--- testing2.py
+++ testing2.py
@@ -1,8 +1,21 @@
+from asyncio import wait
+
 from ocrfunc import ocr
 from picamera2 import Picamera2, Preview
 from PIL import Image
+import os
 import time
 import cv2
+
+def wait_for_file(path, timeout=5):
+    start = time.time()
+    while time.time() - start < timeout:
+        if os.path.exists(path) and os.path.getsize(path) > 0:
+            return True
+        time.sleep(0.1)
+    return False
+
+
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
@@ -14,10 +27,12 @@ while not focused:
 print(f"focused is {focused}")
 time.sleep(2)
 picam2.capture_file("temp.jpg")
-
+wait_for_file("temp.jpg")
 image = Image.open("temp.jpg")
 #print(image.shape)
 img = cv2.imread("image.png", 0)
 _, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 cv2.imwrite("thresh.png", thresh)
 print(f"The image text is:{ocr(image)}")
+
+
